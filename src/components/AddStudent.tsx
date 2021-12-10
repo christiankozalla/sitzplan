e893099fce5376a3cb92da8b1e80799f6200b6fd
Christian Kozalla <christian.kozalla@gmail.com>
2021-12-10 17:59:51 +0100

    Improve responsiveness of AddStudent input

--- src/components/AddStudent.tsx
+++ src/components/AddStudent.tsx
@@ -1,14 +1,8 @@
 import { FC, useState } from "react";
 import { controller } from "../lib/Controller";
-
 import styles from "./AddStudent.module.css";
 
-interface AddStudentProps {
-  x: number;
-  y: number;
-}
-
-export const AddStudent: FC<AddStudentProps> = ({ x, y }) => {
+export const AddStudent: FC = () => {
   const [studentName, setStudentName] = useState("");
 
   const handleNewStudent = () => {
@@ -17,7 +11,7 @@ export const AddStudent: FC<AddStudentProps> = ({ x, y }) => {
   };
 
   return (
-    <div className={styles.wrapper} style={{ left: x + 20, top: y - 30 }}>
+    <div className={styles.addStudent}>
       <label htmlFor="addStudent" className="screenReaderOnly">
         Neuer Schüler
       </label>
@@ -30,7 +24,9 @@ export const AddStudent: FC<AddStudentProps> = ({ x, y }) => {
         onChange={(e) => setStudentName(e.target.value)}
         onKeyDown={(e) => e.key === "Enter" && handleNewStudent()}
       />
-      <button onClick={handleNewStudent}>+</button>
+      <button className={styles.addStudentButton} onClick={handleNewStudent}>
+        +
+      </button>
     </div>
   );
 };
